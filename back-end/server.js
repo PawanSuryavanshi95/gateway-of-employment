d034881d4b97de3ebd877059605b642743125979
PawanSuryavanshi95 <suryavanshi.1@iitj.ac.in>
2020-08-24 00:41:12 +0530

    Added Middlewares to express app

--- back-end/server.js
+++ back-end/server.js
@@ -32,7 +32,7 @@ app.use(
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://www.findpathway.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.findpathway.com');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
