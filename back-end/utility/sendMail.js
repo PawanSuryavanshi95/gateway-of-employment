d10560ac5ab0565a52a0a2d37dfa689f5645ea65
PawanSuryavanshi95 <suryavanshi.1@iitj.ac.in>
2020-09-06 14:58:41 +0530

    Fixed sendMail.js

--- back-end/utility/sendMail.js
+++ back-end/utility/sendMail.js
@@ -1,11 +1,15 @@
 const nodemailer = require('nodemailer');
+const xoauth2 = require('xoauth2');
 
 exports.sendMail = async (to,subject,text) => {
     let transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
+            type: 'OAuth2',
             user: 'goe.cohest@gmail.com',
-            pass: 'R*U#V22$@',
+            clientId: '60558016495-ja64jp4vo5tch3hcf5o8bcd6nf29l66u.apps.googleusercontent.com',
+            clientSecret: 'v6sa-hLJOASmpi7Glbetos67',
+            refreshToken: '1//0feIVCQJR9fWnCgYIARAAGA8SNwF-L9IrRXTKCtZzZ3pcalp4ay57sVX4dYB2fvhp3I2z2e_BWCCMjgdQvV35PPWmbyGR18shFiw',
         }
     });
     
